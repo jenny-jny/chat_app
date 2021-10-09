3a1ff81c8e63c1a45ff03714c77f38b0978b6388
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-09 11:07:04 -0500

    Import GiftedChat and add constructor

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { View, StyleSheet} from 'react-native';
-
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends Component {
+  constructor() {
+    super();
+    this.state = { messages: [] }
+  }
   render() {
     let { name, backgroundColor } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
