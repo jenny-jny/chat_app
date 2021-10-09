08846a80eb02ab3ccf01866e19b122704033594f
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-09 11:25:18 -0500

    Add componentDidMount and onSend methods and render GiftedChat component

--- components/Chat.js
+++ components/Chat.js
@@ -7,11 +7,34 @@ export default class Chat extends Component {
     super();
     this.state = { messages: [] }
   }
+
+  componentDidMount() {
+    this.setState({
+      messages: [{
+        _id: 1,
+        text: 'Hello developer',
+        createdAt = new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      }]
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => {
+      messages: GiftedChat.append(previousState.messages, messages)
+    })
+  }
+
   render() {
     let { name, backgroundColor } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
     return (
       <View style={[styles.container, {backgroundColor: backgroundColor}]}>
+        <GiftedChat messages={this.state.messages} onSend={messages => this.onSend(messages)} user={{_id: 1}}/>
       </View>
     );
   }
